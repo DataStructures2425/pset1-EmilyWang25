b458a36fce291e9e924351c612e735bd2bc855cc
EmilyWang25 <Siyan.Wang@worcesteracademy.org>
2024-10-19 12:15:58 +0800

    Update testpset1.cpp

--- testpset1.cpp
+++ testpset1.cpp
@@ -5,5 +5,22 @@ using namespace std;
 int main()
 {
     init_kalloc();
+    void* ptr = kalloc(1UL<<21);
+    kalloc(1UL<<13);
+    kfree(ptr);
+    void* ptr1 = kalloc(1UL<<13);
+
+    void* ptr2 = kalloc(1UL<<13);
+    kalloc(1UL<<15);
+    kfree(ptr1);
+    kfree(ptr2);
+    kalloc(1UL<<13);
+
+    /*void* ptr = kalloc(1UL<<12);
+    kfree(ptr);
+    kalloc(1UL<<21);*/
+
+
+
 
 }
